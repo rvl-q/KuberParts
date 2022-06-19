cb860b84a732f4a5d3e629ef61100f18757355fd
Ragnar <tsivqdnil.rangar@gmail.com>
2022-06-20 00:49:02 +0300

    kuber 109

--- ping-pong/app/app.js
+++ ping-pong/app/app.js
@@ -1,35 +1,20 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
 import { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
-// import * as todoController from "./controllers/todoController.js";
 
-// configure({
-//   views: `${Deno.cwd()}/views/`,
-// });
 
 const PORT = Deno.env.get("PORT");
 
-// const handleRequest = (request) => {
-//   console.log(`Request to ${request.url}`);
-//   const url = new URL(request.url);
-//   // return new Response("Hello world from port: " + PORT);
-//   if (url.pathname === "/" && request.method === "GET") {
-//     // return await todoController.viewTodos(request); // syntax error?
-//     return todoController.viewTodos(request, PORT);
-//   } else {
-//     return new Response("Not found", { status: 404 });
-//   }
-// };
+let _ping_num = -1
 
 const handleRequest = (request) => {
   console.log(`Request to ${request.url}`);
   const url = new URL(request.url);
-  return new Response("Ping pong world from port: " + PORT);
-  // if (url.pathname === "/" && request.method === "GET") {
-  //   // return await todoController.viewTodos(request); // syntax error?
-  //   return todoController.viewTodos(request, PORT);
-  // } else {
-  //   return new Response("Not found", { status: 404 });
-  // }
+  if (url.pathname === "/pingpong" && request.method === "GET") {
+    _ping_num += 1;
+    return new Response("pong " + _ping_num);
+  } else {
+    return new Response("Not found", { status: 404 });
+  }
 };
 
 console.log("Server started in port " + PORT);

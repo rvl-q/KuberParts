960f06054ed55817411d6e5bf38d87541c5f9508
Ragnar <tsivqdnil.rangar@gmail.com>
2022-06-24 11:34:58 +0300

    working with hard coded url

--- ping-pong/app/app.js
+++ ping-pong/app/app.js
@@ -1,23 +1,21 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
-// import { configure } from "https://deno.land/x/eta@v1.12.3/mod.ts";
-
 
 const PORT = Deno.env.get("PORT");
 
 let _ping_num = 0
-Deno.writeTextFile("./shared/pongs.txt", _ping_num);
+// Deno.writeTextFile("./shared/pongs.txt", _ping_num);
 
 const handleRequest = (request) => {
   console.log(`Request to ${request.url}`);
   const url = new URL(request.url);
   if (url.pathname === "/pingpong" && request.method === "GET") {
     _ping_num += 1;
-    Deno.writeTextFile("./shared/pongs.txt", _ping_num);
+    // Deno.writeTextFile("./shared/pongs.txt", _ping_num);
     return new Response("pong " + _ping_num);
   }
-  //  else {
-  //   return new Response("Not found", { status: 404 });
-  // }
+   else if (url.pathname === "/pongs" && request.method === "GET"){
+    return new Response(_ping_num);
+  }
 };
 
 console.log("Server started in port " + PORT);

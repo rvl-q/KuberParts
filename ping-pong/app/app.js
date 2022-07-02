b674fa846187e4e3b475897ecf82fe69627afd93
Ragnar <tsivqdnil.rangar@gmail.com>
2022-07-02 18:34:06 +0300

    namespace, test database

--- ping-pong/app/app.js
+++ ping-pong/app/app.js
@@ -1,6 +1,23 @@
 import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+import { Client } from "https://deno.land/x/postgres@v0.14.3/mod.ts";
 
 const PORT = Deno.env.get("PORT");
+const PASSWORD = Deno.env.get("POSTGRES_PASSWORD");
+console.log(PASSWORD.length)
+
+const client = new Client({
+  hostname: "postgres-ss-0",
+  database: "postgres",
+  user: "posegres",
+  password: PASSWORD,
+  port: 5432,
+});
+
+await client.connect();
+const result = await client.queryArray("SELECT * FROM names;");
+await client.end();
+
+console.log(result.rows);
 
 let _ping_num = 0
 // Deno.writeTextFile("./shared/pongs.txt", _ping_num);

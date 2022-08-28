5cd5cab464bba6ed1881784a05a4065ffed19bcd
Ragnar <tsivqdnil.rangar@gmail.com>
2022-08-28 15:43:27 +0300

    dummysite

--- dummysite/controller/index.js
+++ dummysite/controller/index.js
@@ -1,148 +1,203 @@
 const k8s = require('@kubernetes/client-node')
-const mustache = require('mustache')
-const request = require('request')
 const JSONStream = require('json-stream')
+const request = require('request')
+const mustache = require('mustache')
 const fs = require('fs').promises
 
-// Use Kubernetes client to interact with Kubernetes
-
-const timeouts = {}
-
 const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
 
-process.env.NODE_ENV === 'development' ? kc.loadFromDefault() : kc.loadFromCluster()
+const opts = {};
+kc.applyToRequest(opts);
 
-const opts = {}
-kc.applyToRequest(opts)
-
-const client = kc.makeApiClient(k8s.CoreV1Api);
+// console.log(kc)
 
 const sendRequestToApi = async (api, method = 'get', options = {}) => new Promise((resolve, reject) => request[method](`${kc.getCurrentCluster().server}${api}`, {...opts, ...options, headers: { ...options.headers, ...opts.headers }}, (err, res) => err ? reject(err) : resolve(JSON.parse(res.body))))
 
-const fieldsFromCountdown = (object) => ({
-  countdown_name: object.metadata.name,
-  container_name: object.metadata.name,
-  job_name: `${object.metadata.name}-job-${object.spec.length}`,
-  namespace: object.metadata.namespace,
-  delay: object.spec.delay,
-  image: object.spec.image,
-  length: object.spec.length
-})
-
-const fieldsFromJob = (object) => ({
-  countdown_name: object.metadata.labels.countdown,
-  container_name: object.metadata.labels.countdown,
-  job_name: `${object.metadata.labels.countdown}-job-${object.metadata.labels.length}`,
-  namespace: object.metadata.namespace,
-  delay: object.metadata.labels.delay,
-  image: object.spec.template.spec.containers[0].image,
-  length: object.metadata.labels.length
-})
-
-const getJobYAML = async (fields) => {
-  const deploymentTemplate = await fs.readFile("job.mustache", "utf-8")
-  return mustache.render(deploymentTemplate, fields)
-}
-
-const jobForCountdownAlreadyExists = async (fields) => {
-  const { countdown_name, namespace } = fields
-  const { items } = await sendRequestToApi(`/apis/batch/v1/namespaces/${namespace}/jobs`)
-
-  return items.find(item => item.metadata.labels.countdown === countdown_name)
-}
-
-const createJob = async (fields) => {
-  console.log('Scheduling new job number', fields.length, 'for countdown', fields.countdown_name, 'to namespace', fields.namespace)
-
-  const yaml = await getJobYAML(fields)
-
-  return sendRequestToApi(`/apis/batch/v1/namespaces/${fields.namespace}/jobs`, 'post', {
-    headers: {
-      'Content-Type': 'application/yaml'
-    },
-    body: yaml
-  })
-}
-
-const removeJob = async ({ namespace, job_name }) => {
-  const pods = await sendRequestToApi(`/api/v1/namespaces/${namespace}/pods/`)
-  pods.items.filter(pod => pod.metadata.labels['job-name'] === job_name).forEach(pod => removePod({ namespace, pod_name: pod.metadata.name }))
-
-  return sendRequestToApi(`/apis/batch/v1/namespaces/${namespace}/jobs/${job_name}`, 'delete')
-}
-
-const removeCountdown = ({ namespace, countdown_name }) => sendRequestToApi(`/apis/stable.dwk/v1/namespaces/${namespace}/countdowns/${countdown_name}`, 'delete')
-
-const removePod = ({ namespace, pod_name }) => sendRequestToApi(`/api/v1/namespaces/${namespace}/pods/${pod_name}`, 'delete')
-
-const cleanupForCountdown = async ({ namespace, countdown_name }) => {
-  console.log('Doing cleanup')
-  clearTimeout(timeouts[countdown_name])
-
-  const jobs = await sendRequestToApi(`/apis/batch/v1/namespaces/${namespace}/jobs`)
-  jobs.items.forEach(job => {
-    if (!job.metadata.labels.countdown === countdown_name) return
-
-    removeJob({ namespace, job_name: job.metadata.name })
-  })
-}
-
-const rescheduleJob = (jobObject) => {
-  const fields = fieldsFromJob(jobObject)
-  if (Number(fields.length) <= 1) {
-    console.log('Countdown ended. Removing countdown.')
-    return removeCountdown(fields)
+const getIngYAML = async (fields) => {
+    const deploymentTemplate = await fs.readFile("ingress.mustache", "utf-8")
+    return mustache.render(deploymentTemplate, fields)
   }
 
-  // Save timeout so if the countdown is suddenly removed we can prevent execution (removing countdown removes job)
-  timeouts[fields.countdown_name] = setTimeout(() => {
-    removeJob(fields)
-    const newLength = Number(fields.length) - 1
-    const newFields = {
-      ...fields,
-      job_name: `${fields.container_name}-job-${newLength}`,
-      length: newLength
-    }
-    createJob(newFields)
-  }, Number(fields.delay))
+  const getDefIngYAML = async (fields) => {
+    const deploymentTemplate = await fs.readFile("defaultingress.mustache", "utf-8")
+    return mustache.render(deploymentTemplate, fields)
+  }
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+const k8sDepApi = kc.makeApiClient(k8s.AppsV1Api)
+
+function initStatusLoop() {
+    const stream = new JSONStream()
+    stream.on('data', async ({ type, object }) => {
+        console.log('type', type)
+        if (type === 'ADDED') {
+            const fields = {
+                website_url: object.spec.website_url,
+                name: object.metadata.name,
+                namespace: object.metadata.namespace || 'default',
+                image: object.spec.image,
+                rawnamespace: object.metadata.namespace
+            }
+            try {
+                if (await NoService(fields)) {
+                    console.log('Creating service');
+                    createService(fields)
+                } else {
+                    console.log('Service ok');
+                }
+                if (await NoIngress(fields)) {
+                    console.log('Creating ingress');
+                    createIngress(fields)    
+                } else {
+                    console.log('Ingress ok');
+                }
+                if (await NoDeployment(fields)) {
+                    console.log('Creating deployment');
+                    createDeployment(fields)
+                } else {
+                    console.log('Deployment ok');
+                }
+            } catch (error) {
+                console.log('watch loop error', JSON.stringify(error.stack).length)
+                console.log('error')
+            }
+        } else if (type === 'DELETED') {
+            const fields = {
+                website_url: object.spec.website_url,
+                name: object.metadata.name,
+                namespace: object.metadata.namespace || 'default',
+                image: object.spec.image,
+            }
+            try {
+                console.log('else')
+                removeDeployment(fields)
+                console.log('removed')
+            } catch (error) {
+                console.log('watch loop DELETE error', JSON.stringify(error))
+                console.log('error')
+            }
+        }
+    })
+    request.get(`${kc.getCurrentCluster().server}/apis/stable.dwk/v1/dummysites?watch=true`, opts).pipe(stream)
 }
 
-const maintainStatus = async () => {
-  (await client.listPodForAllNamespaces()).body // A bug in the client(?) was fixed by sending a request and not caring about response
-
-  /**
-   * Watch Countdowns
-   */
-
-  const countdown_stream = new JSONStream()
-
-  countdown_stream.on('data', async ({ type, object }) => {
-    const fields = fieldsFromCountdown(object)
-
-    if (type === 'ADDED') {
-      if (await jobForCountdownAlreadyExists(fields)) return // Restarting application would create new 0th jobs without this check
-      createJob(fields)
-    }
-    if (type === 'DELETED') cleanupForCountdown(fields)
-  })
-
-  request.get(`${kc.getCurrentCluster().server}/apis/stable.dwk/v1/countdowns?watch=true`, opts).pipe(countdown_stream)
-
-  /**
-   * Watch Jobs
-   */
-
-  const job_stream = new JSONStream()
-
-  job_stream.on('data', async ({ type, object }) => {
-    if (!object.metadata.labels.countdown) return // If it's not countdown job don't handle
-    if (type === 'DELETED' || object.metadata.deletionTimestamp) return // Do not handle deleted jobs
-    if (!object?.status?.succeeded) return
-
-    rescheduleJob(object)
-  })
-
-  request.get(`${kc.getCurrentCluster().server}/apis/batch/v1/jobs?watch=true`, opts).pipe(job_stream)
+const NoService = async (fields) => {
+    const { namespace } = fields
+    const { items } = await sendRequestToApi(`/api/v1/namespaces/${namespace}/services`)
+    // console.log('NoSer 007', items)
+    if (!items) { return true } else {console.log('NoSer 3',(items.find(item => item.metadata.name === `${fields.name}-svc`))) ; return typeof (items.find(item => item.metadata.name === `${fields.name}-svc`)) === 'undefined' }
 }
 
-maintainStatus()
+const NoIngress = async (fields) => {
+    // console.log('NoIng 1');
+    const { name, namespace } = fields
+    const { items } = await sendRequestToApi(`/apis/networking.k8s.io/v1/namespaces/${namespace}/ingresses`)
+    // console.log('NoIng 007', items)
+    if (!items) { console.log('NoIng 2'); return true } else { console.log('NoIng 3', (items.find(item => item.metadata.name === `${name}-ing`)) ); return typeof (items.find(item => item.metadata.name === `${name}-ing`)) === 'undefined' }
+}
+  
+const NoDeployment = async (fields) => {
+    // console.log('NoDep 1')
+    const { name, namespace } = fields
+    const { items } = await sendRequestToApi(`/apis/apps/v1/namespaces/${namespace}/deployments`)
+    // console.log('NoDep 007', items)
+    if (!items) { console.log('NoDep 2'); return true } else { console.log('NoDep 3', (items.find(item => item.metadata.name === `${fields.name}-dep`))); return typeof (items.find(item => item.metadata.name === `${name}-dep`)) === 'undefined' }
+}
+
+async function createService(fields) {
+    await k8sApi.createNamespacedService(fields.namespace, {
+        apiVersions: 'v1',
+        kind: 'Service',
+        metadata: {
+            name: `${fields.name}-svc`,
+        },
+        spec: {
+            type: 'ClusterIP',
+            selector: {
+                app: fields.name,
+            },
+            ports: [{
+                port: 2345,
+                protocol: 'TCP',
+                targetPort: 3000,
+            }]
+        }
+    }).catch(error => {
+        console.log('svc create error')
+        console.log(JSON.stringify(error, ["message", "response"]));
+    })
+}
+
+async function createIngress(fields) {
+    // let yaml
+    const yaml = fields.namespace === 'default' ? await getDefIngYAML(fields) : await getIngYAML(fields)
+    console.log('mustaassi:\n', yaml)
+    sendRequestToApi(`/apis/networking.k8s.io/v1/namespaces/${fields.namespace}/ingresses`, 'post', {
+        headers: {
+          'Content-Type': 'application/yaml'
+        },
+        body: yaml
+    }).catch(error => {
+        console.log('ingress creating error')
+        console.log(JSON.stringify(error));
+    })
+}
+
+async function createDeployment(fields) {
+    await k8sDepApi.createNamespacedDeployment(fields.namespace, {
+        apiVersion: 'apps/v1',
+        kind: 'Deployment',
+        metadata: {
+            name: `${fields.name}-dep`,
+            namespace: `${fields.namespace}`
+        },
+        spec: {
+            selector: {
+                matchLabels: {
+                    app: fields.name
+                }
+            },
+            template: {
+                metadata: {
+                    labels: {
+                        app: fields.name
+                    }
+                },
+                spec: {
+                    containers: [{
+                        name: 'dummysite-app',
+                        image: `${fields.image}`,
+                        env: [{
+                            name: 'URL',
+                            value: fields.website_url
+                        }]
+                    }]
+                }
+            }
+        }
+    }).catch(error => {
+        console.log('create deployment error')
+        console.log(JSON.stringify(error, ["message", "response"]));
+    })
+}
+
+
+/*
+deleteNamespacedDeployment(name: string, namespace: string, pretty?: string, dryRun?: string, 
+    gracePeriodSeconds?: number, orphanDependents?: boolean, propagationPolicy?: string, 
+    body?: V1DeleteOptions, options?: { headers: {} }): Promise<{ body: V1Status; response: IncomingMessage }>
+
+deleteNamespacedDeployment("nginx", NAMESPACE, null, null, null, null, null, null);
+*/
+async function removeDeployment(fields) {
+    await k8sDepApi.deleteNamespacedDeployment( `${fields.name}-dep`,
+        fields.namespace,
+        null, null, null, null, null, null
+    ).catch(error => {
+        console.log('deployment remove error')
+        console.log(JSON.stringify(error));
+    })
+}
+
+initStatusLoop()
